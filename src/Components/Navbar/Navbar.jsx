cc195732fe308e8da66784f380e1bef24706eb38
unknown <emonhossen1000@gmail.com>
2023-10-12 12:13:01 +0600

    logo added

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
     return (
 <nav className="navbar  md:w-11/12 md:mx-auto flex-col  md:flex-row md:justify-between md:bg-base-100 ">
   <div className="navbar-start">
-    <img className="w-[220px]" src="/public/Logo.png" alt="" />
+    <img className="w-[220px]" src="https://i.ibb.co/5nH7JMH/Logo.png" alt="" />
   </div>
   <div className="navbar-center    ">
     <ul className=" flex gap-6 menu-horizontal px-1 pt-5">
