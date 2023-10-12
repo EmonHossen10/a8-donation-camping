9737b51d3d316660ffb6f6c6d65f4f8421dd5b78
unknown <emonhossen1000@gmail.com>
2023-10-12 12:16:42 +0600

    all console remove

--- src/Components/Cards/Cards.jsx
+++ src/Components/Cards/Cards.jsx
@@ -1,7 +1,7 @@
 import Card from "../Card/Card";
 
 const Cards = ({ category }) => {
-  console.log(category)
+   
   return (
     <>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5 w-11/12 mx-auto my-10">
